b71b6247b72a1fa8c3b6f351f011b53fe9ef1683
MateiuAdelinaSonia <adelinasonia.mateiu@yahoo.com>
2021-05-22 22:37:05 +0300

    Add education title.

--- src/App.js
+++ src/App.js
@@ -6,41 +6,56 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     paper: {
-        width: 416,
+        width: 380,
         height: 60,
         backgroundColor: "#fff",
-        alignSelf: "center",
     },
     large: {
-        width: theme.spacing(15),
-        height: theme.spacing(15),
+        width: theme.spacing(18),
+        height: theme.spacing(18),
     },
 }));
 
 function App() {
     const classes = useStyles();
     return (
-        <Container maxWidth="md" style={{ backgroundColor: '#bad4d1', height: '1500px', width: '2000px'}}>
+        <Container maxWidth="md" style={{ backgroundColor: '#d9dede', height: '1500px', width: '2000px'}}>
 
             <Grid container spacing={10}>
 
-                <Grid item xs={12}>
-                    <div className={classes.root}>
-                        <Avatar alt="Mateiu Adelina-Sonia" src="../myPicture.jpg" className={classes.large} />
-                    </div>
-                </Grid>
+                <Container maxWidth="md" style={{backgroundColor: 'rgba(123,70,134,0.91)', height: '1500px', width: '460px'}}>
 
-                <Grid item xs={6}>
-                    <Paper className={classes.paper} />
-                </Grid>
+                    <Grid item xs={6} style={{marginTop: '50px'}}>
+                        <div className={classes.root}>
+                            <Avatar alt="Mateiu Adelina-Sonia" src="../myPicture.jpg" className={classes.large} />
+                        </div>
+                    </Grid>
 
-                <Grid item xs={6}>
-                    <Paper className={classes.paper} />
+                    <Grid item xs={6} style={{marginTop: '35px', marginLeft: '14px'}}>
+                        <Paper align="center" className={classes.paper}>
+                            <img src="../graduation.png" width='30' hight='30' style={{marginTop: '10px'}}/>
+                            <typography style={{fontSize: 30, fontWeight: 'bold', color: '#1f0e0d'}}>  Education</typography> </Paper>
+                    </Grid>
+
+                    <Grid item xs={6} style={{marginTop: '35px', marginLeft: '14px'}}>
+                        <Paper align="center" className={classes.paper}>  </Paper>
+                    </Grid>
+
+                    <Grid item xs={6} style={{marginTop: '35px', marginLeft: '14px'}}>
+                        <Paper align="center" className={classes.paper}>  </Paper>
+                    </Grid>
+
+                    <Grid item xs={6} style={{marginTop: '35px', marginLeft: '14px'}}>
+                        <Paper align="center" className={classes.paper}>  </Paper>
+                    </Grid>
+
+                </Container>
+
+                <Grid align="center" item xs={6} style={{ marginTop: '188px', marginRight: '20px'}}>
+                    <Paper align="center" className={classes.paper} />
                 </Grid>
 
             </Grid>
-
-
 
         </Container>
 
